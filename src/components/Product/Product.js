f7a8f4b1f06751c1360840ca0b749cbcb458153f
MarcinDraus <marcin.drauss@gmail.com>
2023-06-23 00:20:44 +0200

    addToCart

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -31,12 +31,14 @@ const Product = ({ title, basePrice, colors, sizes, name }) => {
 
   const summary = (e) => {
     e.preventDefault();
+    console.log('summary');
     const summary = {
       name: title,
       price: getPrice(),
       size: currentSize,
       color: currentColor,
     };
+    
     console.log(summary);
   };
 
@@ -82,15 +84,12 @@ const Product = ({ title, basePrice, colors, sizes, name }) => {
                     )}
                     onClick={() => handlerColor(color)}
                   ></button>
-                  
                 </li>
-                
               ))}
-              
             </ul>
           </div>
-          <Button className={styles.button}>
-            <span className="fa fa-shopping-cart" onClick={(e) => summary(e)} />
+          <Button className={styles.button} onClick={summary}>
+            <span className="fa fa-shopping-cart" />
           </Button>
         </form>
       </div>
@@ -107,53 +106,3 @@ Product.propTypes = {
 };
 
 export default Product;
-
-
-
-
-// import styles from './Product.module.scss';
-// import clsx from 'clsx';
-// import Button from '../Button/Button';
-
-// const Product = props => {
-//   return (
-//     <article className={styles.product}>
-//       <div className={styles.imageContainer}>
-//         <img 
-//           className={styles.image}
-//           alt="Kodilla shirt"
-//           src={`${process.env.PUBLIC_URL}/images/products/shirt-kodilla--black.jpg`} />
-//       </div>
-//       <div>
-//         <header>
-//           <h2 className={styles.name}>Kodilla shirt</h2>
-//           <span className={styles.price}>Price: 20$</span>
-//         </header>
-//         <form>
-//           <div className={styles.sizes}>
-//             <h3 className={styles.optionLabel}>Sizes</h3>
-//             <ul className={styles.choices}>
-//               <li><button type="button" className={styles.active}>S</button></li>
-//               <li><button type="button">M</button></li>
-//               <li><button type="button">L</button></li>
-//               <li><button type="button">XL</button></li>
-//             </ul>
-//           </div>
-//           <div className={styles.colors}>
-//             <h3 className={styles.optionLabel}>Colors</h3>
-//             <ul className={styles.choices}>
-//               <li><button type="button" className={clsx(styles.colorBlack, styles.active)} /></li>
-//               <li><button type="button" className={clsx(styles.colorRed)} /></li>
-//               <li><button type="button" className={clsx(styles.colorWhite)} /></li>
-//             </ul>
-//           </div>
-//           <Button className={styles.button}>
-//             <span className="fa fa-shopping-cart" />
-//           </Button>
-//         </form>
-//       </div>
-//     </article>
-//   )
-// };
-
-// export default Product;
