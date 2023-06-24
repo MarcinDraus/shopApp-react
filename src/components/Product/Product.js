4fea5d1f825af19997213dec132c5be6448296c1
MarcinDraus <marcin.drauss@gmail.com>
2023-06-24 13:00:07 +0200

    rm -rf node_modules

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,3 +1,86 @@
+
+// import React, { useState, useMemo } from 'react';
+// import PropTypes from 'prop-types';
+// import ProductImage from './ProductImage/ProductImage';
+// import ProductForm from './ProductFrom/ProductFrom';
+// import styles from './Product.module.scss';
+
+// const Product = ({ title, basePrice, colors, sizes, name }) => {
+//   const [currentColor, setCurrentColor] = useState(colors[0]);
+//   const [currentSize, setCurrentSize] = useState(sizes[0].name);
+
+//   const handlerColor = (color) => {
+//     setCurrentColor(color);
+//     console.log(color, 'color');
+//   };
+
+//   const handlerSize = (size) => {
+//     setCurrentSize(size);
+//   };
+
+//   const prepareColorClassName = (color) => {
+//     return styles['color' + color[0].toUpperCase() + color.substr(1).toLowerCase()];
+//   };
+
+//   const getPrice = useMemo(() => {
+//     return basePrice + sizes.find(size => size.name === currentSize).additionalPrice
+//   }, [basePrice, currentSize, sizes]);
+
+//   // const getPrice = useMemo(() => {
+//   //   const selectedSize = sizes.find((size) => size.name === currentSize);
+//   //   if (selectedSize) {
+//   //     return basePrice + selectedSize.additionalPrice;
+//   //   }
+//   //   return basePrice;
+//   // }, [currentSize, basePrice, sizes]);
+
+//   const summary = (e) => {
+//     e.preventDefault();
+//     const summaryData = {
+//       name: name,
+//       title: title,
+//       totalPrice: getPrice,
+//       size: currentSize,
+//       color: currentColor,
+//     };
+//     console.log('summary', summary);
+//     console.log(summaryData, 'summaryData');
+//   };
+
+//   return (
+//     <article className={styles.product}>
+//       <ProductImage name={name} title={title} currentColor={currentColor} />
+//       <div>
+//         <header>
+//           <h2 className={styles.name}>{title}</h2>
+//           <span className={styles.price}>Price: {getPrice()}$</span>
+//         </header>
+//         <ProductForm
+//           sizes={sizes}
+//           colors={colors}
+//           currentSize={currentSize}
+//           currentColor={currentColor}
+//           handlerSize={handlerSize}
+//           handlerColor={handlerColor}
+//           summary={summary}
+//           prepareColorClassName={prepareColorClassName}
+//         />
+//       </div>
+//     </article>
+//   );
+// };
+
+// Product.propTypes = {
+//   title: PropTypes.string.isRequired,
+//   basePrice: PropTypes.number.isRequired,
+//   colors: PropTypes.array.isRequired,
+//   sizes: PropTypes.array.isRequired,
+//   name: PropTypes.string.isRequired,
+// };
+
+// export default Product;
+
+
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useState } from 'react';
@@ -43,13 +126,6 @@ const Product = ({ title, basePrice, colors, sizes, name }) => {
     console.log(summaryData, 'summaryData');
   };
 
-  // useEffect(() => {
-  //   summary();
-  // // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, []); // Empty dependency array to run the effect only once
-
-  // // eslint-disable-next-line no-unused-vars
-  // const shirtImageUrl = `${process.env.PUBLIC_URL}/images/products/shirt-${name}--${currentColor}.jpg`;
 
   return (
     <article className={styles.product}>
