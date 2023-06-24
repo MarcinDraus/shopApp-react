1a9f14ddc74c2093fea791e5590ea2728ca4b4c7
MarcinDraus <marcin.drauss@gmail.com>
2023-06-24 02:36:50 +0200

    summary-ready separate components

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,8 +1,9 @@
-import styles from './Product.module.scss';
-import clsx from 'clsx';
-import Button from '../Button/Button';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { useState } from 'react';
+import ProductImage from './ProductImage/ProductImage';
+import ProductForm from './ProductFrom/ProductFrom';
+import styles from './Product.module.scss';
 
 const Product = ({ title, basePrice, colors, sizes, name }) => {
   const [currentColor, setCurrentColor] = useState(colors[0]);
@@ -29,9 +30,6 @@ const Product = ({ title, basePrice, colors, sizes, name }) => {
     return basePrice;
   };
 
-  // eslint-disable-next-line no-unused-vars
-  const [summaryInfo, setSummaryInfo] = useState(null);//skasowac
-
   const summary = (e) => {
     e.preventDefault();
     const summaryData = {
@@ -41,63 +39,36 @@ const Product = ({ title, basePrice, colors, sizes, name }) => {
       size: currentSize,
       color: currentColor,
     };
-    console.log(summary);
-    console.log(summaryData);
+    console.log('summary', summary);
+    console.log(summaryData, 'summaryData');
   };
 
-  const shirtImageUrl = `${process.env.PUBLIC_URL}/images/products/shirt-${name}--${currentColor}.jpg`;
+  // useEffect(() => {
+  //   summary();
+  // // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, []); // Empty dependency array to run the effect only once
+
+  // // eslint-disable-next-line no-unused-vars
+  // const shirtImageUrl = `${process.env.PUBLIC_URL}/images/products/shirt-${name}--${currentColor}.jpg`;
 
   return (
-
-
-    
     <article className={styles.product}>
-      <div className={styles.imageContainer}>
-        <img className={styles.image} alt={title} src={shirtImageUrl} />
-      </div>
+      <ProductImage name={name} title={title} currentColor={currentColor} />
       <div>
         <header>
           <h2 className={styles.name}>{title}</h2>
           <span className={styles.price}>Price: {getPrice()}$</span>
         </header>
-        <form>
-          <div className={styles.sizes}>
-            <h3 className={styles.optionLabel}>Sizes</h3>
-            <ul className={styles.choices}>
-              {sizes.map((size) => (
-                <li key={size.name}>
-                  <button 
-                    type="button"
-                    className={clsx(currentSize === size.name && styles.active)}
-                    onClick={() => handlerSize(size.name)}
-                  >
-                    {size.name}
-                  </button>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className={styles.colors}>
-            <h3 className={styles.optionLabel}>Colors</h3>
-            <ul className={styles.choices}>
-              {colors.map((color) => (
-                <li key={color}>
-                  <button
-                    type="button"
-                    className={clsx(
-                      prepareColorClassName(color),
-                      currentColor === color && styles.active
-                    )}
-                    onClick={() => handlerColor(color)}
-                  ></button>
-                </li>
-              ))}
-            </ul>
-          </div>
-          <Button className={styles.button} onClick={summary}>
-            <span className="fa fa-shopping-cart" />
-          </Button>
-        </form>
+        <ProductForm
+          sizes={sizes}
+          colors={colors}
+          currentSize={currentSize}
+          currentColor={currentColor}
+          handlerSize={handlerSize}
+          handlerColor={handlerColor}
+          summary={summary}
+          prepareColorClassName={prepareColorClassName}
+        />
       </div>
     </article>
   );
@@ -112,3 +83,4 @@ Product.propTypes = {
 };
 
 export default Product;
+
--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import App from './App';
 import './styles/normalize.scss';
 import './styles/global.scss';
 import 'font-awesome/css/font-awesome.min.css';
+import '../src/styles/settings.scss';
+//import '../../styles/normalize.scss';
+import '../src/styles/normalize.scss';
+import '../src/styles/global.scss';
 
 ReactDOM.render(
   <React.StrictMode>
